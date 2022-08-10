1b1b7ee24e1f2478b362418039346b01fc3203c2
navtsingh <navtsingh@ladmins-MacBook-Pro.local>
2022-08-10 16:35:08 -0700

    nginx added

--- movie-api/src/index.js
+++ movie-api/src/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.json({
     version: packageJson.version,
   });
